a355cfdd0ff62be0b52c07b22d55322043975ee1
Carlos Agudo <carlosagudopos@gmail.com>
2021-01-30 20:42:40 +0100

    push

--- Assets/Scripts/ChangeCameras.cs
+++ Assets/Scripts/ChangeCameras.cs
@@ -30,6 +30,7 @@ public class ChangeCameras : MonoBehaviour
     [SerializeField]
     public Transform puntoFinal;
 
+    private bool puesbools=false;
     public void Start()
     {
         input.canMove = true;
@@ -43,29 +44,39 @@ public class ChangeCameras : MonoBehaviour
         if (globalCamera) {
             this.transform.position = new Vector3(input.Player1.transform.position.x, input.Player1.transform.position.y, this.transform.position.z);
         }
+
         if (Input.GetKeyDown(KeyCode.E) && timer.canChange)
         {
+ 
             timer.inciarContador = !timer.inciarContador;
             input.canMove = !input.canMove;
             globalCamera = !globalCamera;
             if (globalCamera)
             {
                 StartCoroutine(Transition(defaultvalueNear, defaultvalueLigth, input.Player1.transform));
-                //StartCoroutine(ExampleCoroutine());
-                //StartCoroutine(Transition2(input.Player1.transform));
-            }
-            else {
-                StartCoroutine(Transition(nearest, intensidadGlobal, puntoFinal));
-                //StartCoroutine(ExampleCoroutine());
-                //StartCoroutine(Transition2(puntoFinal));
-                //this.GetComponent<Camera>().
-            }
-         
-        }
+                puesbools = true;
+                if (puesbools)
+                {
+                    StartCoroutine(Transition2(input.Player1.transform));
+                    puesbools = false;
+                }
 
+            }
+            else
+            {
+                StartCoroutine(Transition(nearest, intensidadGlobal, puntoFinal));
+                puesbools = true;
+                if (puesbools)
+                {
+                    StartCoroutine(Transition2(puntoFinal));
+                    puesbools = false;
+                }
+            }
+        }
+       
     }
 
-    IEnumerator Transition(float value, float intensity, Transform ttt)
+    IEnumerator Transition(float value, float intensity, Transform tt)
     {
         float t = 0.0f;
         float ini = this.GetComponent<Camera>().orthographicSize;
@@ -76,21 +87,24 @@ public class ChangeCameras : MonoBehaviour
             t += Time.deltaTime * (Time.timeScale / transitionDuration);
             this.GetComponent<Camera>().orthographicSize = Mathf.Lerp(ini, value, t);
             GameObject.FindGameObjectWithTag("GlobalLigth").GetComponent<Light2D>().intensity = Mathf.Lerp(ints, intensity, t);
+            
             yield return 0;
-
         }
-      
+        yield return 0;
     }
-        //float t = 0.0f;
-        //float startingPosX = this.transform.position.x;
-        //float startingPosY = this.transform.position.y;
-        //while (t < 1.0f)
-        //{
-        //    t += Time.deltaTime * (Time.timeScale / 5);
-        //    float x = Mathf.Lerp(startingPosX, tt.position.x, t);
-        //    float y = this.transform.position.y;
-        //    this.transform.position = new Vector3(x, y, -10);
-        //    yield return 0;
-        //}
+    IEnumerator Transition2(Transform ttt)
+    {
+        float t = 0.0f;
+        float startingPosX = this.transform.position.x;
+        float startingPosY = this.transform.position.y +10;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * (Time.timeScale / transitionDuration);
+            float x = Mathf.Lerp(startingPosX, ttt.position.x, t);
+            float y = this.transform.position.y;
+            this.transform.position = new Vector3(x, y, -10);
+            yield return 0;
+        }
+    }
 
 }
